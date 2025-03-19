cccdf966b4220d6c8d3bcd3997b83cac4a7b8e2e
Marcelrp <marcel.pie@hotmail.com>
2025-03-19 10:32:59 -0300

    first project in spring name of project is Greeting!

---
+++ rest-with-spring-boot-and-java/src/main/java/mrp/rest_with_spring_boot_and_java/StartUp.java
@@ -0,0 +1,13 @@
+package mrp.rest_with_spring_boot_and_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUp.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-and-java/src/main/java/mrp/rest_with_spring_boot_and_java/model/Greeting.java
@@ -0,0 +1,3 @@
+package mrp.rest_with_spring_boot_and_java.model;
+
+public record Greeting(long id, String content) {}
